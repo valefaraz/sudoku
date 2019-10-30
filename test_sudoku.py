f72ec0efa18b7960ff6d6d2131f47bb3c9485800
valefaraz <valefaraz@gmail.com>
2019-10-30 13:57:56 -0300

    comit local

--- test_sudoku.py
+++ test_sudoku.py
@@ -158,9 +158,59 @@ class TestSudoku(unittest.TestCase):
     
     def test_control_general_1(self):
         self.assertEqual(self.game.control_general(0,2,"1"), True)
-    
-    def test_control_general_2(self):
-        self.assertEqual(self.game.control_general(0,0,"5"), False)
+
+    @parameterized.expand([
+        (3, 5, '5'),
+        (2, 5, '9'),
+        (8, 5, '3'),
+        (7, 5, '6'),
+        (4, 5, '7'),
+        (8, 6, '2'),
+        (0, 6, '2'),
+        (4, 6, '3'),
+        (2, 6, '9'),
+        (6, 6, '5'),
+        (3, 6, '8'),
+        (3, 0, '2'),
+        (5, 0, '3'),
+        (7, 0, '5'),
+        (8, 0, '6'),
+        (0, 0, '7'),
+        (6, 1, '1'),
+        (1, 1, '6'),
+        (0, 1, '3'),
+        (2, 1, '6'),
+        (3, 1, '7'),
+        (7, 1, '5'),
+        (2, 2, '3'),
+        (3, 2, '4'),
+        (1, 2, '5'),
+        (4, 2, '1'),
+        (6, 2, '8'),
+        (4, 3, '1'),
+        (8, 3, '9'),
+        (6, 3, '8'),
+        (0, 3, '5'),
+        (3, 3, '6'),
+        (1, 3, '7'),
+        (1, 4, '1'),
+        (5, 4, '2'),
+        (4, 4, '4'),
+        (6, 4, '6'),
+        (1, 7, '7'),
+        (7, 7, '1'),
+        (6, 7, '2'),
+        (5, 7, '6'),
+        (2, 7, '7'),
+        (2, 8, '5'),
+        (3, 8, '1'),
+        (4, 8, '2'),
+        (1, 8, '3'),
+        (5, 8, '5'),
+        (0, 8, '6'),
+    ])
+    def test_control_general_2(self,fila,columna,valor):
+        self.assertFalse(self.game.control_general(fila,columna,valor), False)
     
     def test_write9_1(self):
         self.assertEqual(self.game.write(0,2,"1"), "1")
